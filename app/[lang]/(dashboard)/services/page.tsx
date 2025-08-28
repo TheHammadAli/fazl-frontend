065c3fc0e971198c50106859a7fa78057ea148b6
Smad-ul-hassan <smadhssn@gmail.com>
2025-08-28 13:55:18 +0500

    service ui components

--- app/[lang]/(dashboard)/services/page.tsx
+++ app/[lang]/(dashboard)/services/page.tsx
@@ -1,7 +1,12 @@
+import Services from "@/components/Services/Services";
 import React from "react";
 
 function page() {
-  return <div>Services</div>;
+  return (
+    <div>
+      <Services />
+    </div>
+  );
 }
 
 export default page;
---
+++ components/Services/ChooseVideoTab.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useDictionary } from "@/dictionaries/DictionaryProvider";
+import React, { useState } from "react";
+import Image from "next/image";
+import crossImage from "@/assets/icons/cross-icon.svg";
+
+function ChooseVideoTab() {
+  const { pages, placeholders } = useDictionary();
+  const [file, setFile] = useState<File | null>(null);
+
+  const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) return;
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
+  const removeVideo = () => {
+    setFile(null);
+  };
+
+  return (
+    <div>
+      <div className="mt-5 flex gap-2 flex-wrap">
+        {/* Video Preview */}
+        {file && (
+          <div className="relative h-[126px] w-[126px] rounded-[12px] overflow-hidden">
+            <video
+              src={URL.createObjectURL(file)}
+              className="object-cover h-full w-full"
+              controls
+            />
+            <div
+              className="absolute h-[24px] w-[24px] right-2 top-2 bg-opacity-50 flex items-center justify-center cursor-pointer"
+              onClick={removeVideo}
+              data-testid="remove-video"
+            >
+              <div className="h-full w-full rounded-full flex items-center justify-center relative overflow-hidden">
+                <div className="h-full w-full absolute bg-white opacity-80 overflow-hidden"></div>
+                <div className="relative">
+                  <Image
+                    src={crossImage}
+                    alt="cross-icon"
+                    className="overflow-hidden"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {/* Upload Button */}
+        {!file && (
+          <div className="h-[126px] min-w-[126px] flex items-center justify-center">
+            <label
+              htmlFor="video-upload"
+              className="h-[46px] border-green-1 border-[1px] px-3 rounded-[12px] flex items-center justify-center gap-1 cursor-pointer"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="#007781"
+                className="size-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 4.5v15m7.5-7.5h-15"
+                />
+              </svg>
+
+              <h1 className="font-medium text-green-1 text-[16px]">
+                {placeholders.upload_video}
+              </h1>
+              <input
+                id="video-upload"
+                type="file"
+                accept="video/*"
+                onChange={handleUpload}
+                className="hidden"
+              />
+            </label>
+          </div>
+        )}
+      </div>
+
+      {/* Counter */}
+      <div className="w-full flex mt-4 items-center justify-center text-[14px] font-normal text-green-2">
+        {file ? "1/1" : "0/1"}
+      </div>
+    </div>
+  );
+}
+
+export default ChooseVideoTab;
